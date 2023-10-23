85eb895a459d11da03be7942470f6cf184adad10
OlgaStude <112942851+OlgaStude@users.noreply.github.com>
2023-10-23 13:42:42 +0400

    Add files via upload

--- app/Http/Controllers/API/complainController.php
+++ app/Http/Controllers/API/complainController.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\categoryRequest;
 use App\Http\Requests\complainRequest;
 use App\Models\Category;
+use App\Models\Complain;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class complainController extends Controller
 {
@@ -22,9 +24,17 @@ class complainController extends Controller
     }
 
 
-    public function addComplain(complainRequest $req){
+    public function addComplaint(Request $req){
 
-        
+        $req->file('img')->store('public/old_ver_imgs');
+        $image = $req->file('img')->hashName();
+
+        $complaint = Complain::create(['name' => $req->name, 'description' => $req->description, 'categories_id' => $req->category_id, 'users_id' => Auth::user()->id, 'image' => $image, 'status' => 'Новая']);
+
+        if($complaint){
+            $success_message = $req->session()->put('success_message', 'Отправлено успешно!');
+            return redirect()->back()->with($success_message);
+        }
 
     }
 
