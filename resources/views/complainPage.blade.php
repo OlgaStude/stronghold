ac004bf9d77b46885e55ae5597aa23f350eb24a9
OlgaStude <112942851+OlgaStude@users.noreply.github.com>
2023-10-23 13:41:25 +0400

    Add files via upload

--- resources/views/complainPage.blade.php
+++ resources/views/complainPage.blade.php
@@ -7,5 +7,25 @@
 </head>
 <body>
     
+    @include('components/header')
+    
+    <form action="{{ route('addcomplaint') }}" method="post" enctype="multipart/form-data">
+    @csrf  
+        <input type="text" name="name" placeholder="Название">
+        <textarea name="description" id="" cols="30" rows="10" placeholder="Описание"></textarea>
+        <select name="category_id" id="">
+            @foreach($categories as $category)
+                <option value="{{ $category->id }}">{{ $category->name }}</option>
+            @endforeach
+        </select>
+        <input type="file" name="img">
+        <button type="submit">Отправить заявку</button>
+        @if(session('success_message') !== null)
+            <p>{{ session('success_message') }}</p>
+            {{ Session::forget('success_message') }}
+        @endif
+    </form>
+
+    
 </body>
 </html>
