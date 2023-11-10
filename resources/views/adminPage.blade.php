7b7bff1194ca2eef39765cf1d2a7446a11b9cafb
OlgaStude <112942851+OlgaStude@users.noreply.github.com>
2023-11-10 11:43:22 +0400

    Add files via upload

--- resources/views/adminPage.blade.php
+++ resources/views/adminPage.blade.php
@@ -3,19 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
     <title>Admin</title>
+    <style>
+        .declination_form{
+            display: none;
+        }
+    </style>
 </head>
 <body>
 @include('components/header')
     
-    <form action="" method="post">
-        <select name="" id="">
+    <div>
+        <select name="" id="categories">
             @foreach($categories as $category)
-                <option value="{{ $category->name }}">{{ $category->name }}</option>
+                <option value="{{ $category->id }}">{{ $category->name }}</option>
             @endforeach
 
         </select>
-    </form>
+        <button onclick="deleteCategory()">Удалить категорию</button>
+    </div>
 
     <form action="{{ route('addcategory') }}" method="post">
     @csrf  
@@ -23,5 +30,68 @@
         <button type="submit">Добавить категорию</button>
     </form>
 
+    <div id="complains_container">
+        @foreach($complains as $complain)
+        <div class="upper_line">
+            <p>{{ $complain->category_name }}</p>
+            <p>{{ $complain->status}}</p>
+            <p>{{ \Carbon\Carbon::parse($complain->created_at)->format('d.m.Y') }}</p>
+        </div>
+        <img src="{{ asset('storage/old_ver_imgs/'. $complain->image_old) }}" alt="">
+        <p>{{ $complain->name }}</p>
+        <p>{{ $complain->description }}</p>
+        <form action="{{ route('solved') }}" enctype="multipart/form-data" method="post">
+        @csrf  
+
+            <button class="solved_btn">Статус "Решено"</button>
+            <input type="file" name="image_new">
+            @error('image_new')
+                <p>{{ $message }}</p>
+            @enderror
+            <input type="text" hidden name="id" value="{{ $complain->complains_id }}">
+        </form>
+        <button class="decline_btn">Отклонить заявку</button>
+
+        <form class="declination_form" action="{{ route('decline') }}" method="post">
+        @csrf  
+
+            <textarea name="reason" placeholder="Причина отклонения"></textarea>
+            <input type="text" hidden name="id" value="{{ $complain->complains_id }}">
+
+            @if(session('fail_message') !== null)
+            <p>{{ session('fail_message') }}</p>
+            {{ Session::forget('fail_message') }}
+        @endif
+            <button class="decline">Отклонить</button>
+        </form>
+        @endforeach
+    </div>
+    
+    <script>
+
+        $('.decline_btn').click(function(){
+            if($(this).next().is(':hidden')){
+                $(this).next().show()
+            } else {
+                $(this).next().hide()
+            }
+        })
+
+        function deleteCategory() {
+            let id = $('#categories').val();
+            $.ajax({
+                    url: '{{ route("deletecategory") }}',
+                    method: 'GET',
+                    data: {
+                        id: id
+                    },
+                    success: function(data) {
+                        console.log(data)
+                    }
+                
+            })
+        }
+        
+    </script>
 </body>
 </html>
