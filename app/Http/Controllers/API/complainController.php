f507e9906d5a5fcd6d04c7b1e028c7a6dfca11b2
OlgaStude <112942851+OlgaStude@users.noreply.github.com>
2023-11-10 11:46:01 +0400

    Add files via upload

--- app/Http/Controllers/API/complainController.php
+++ app/Http/Controllers/API/complainController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\categoryRequest;
 use App\Http\Requests\complainRequest;
+use App\Http\Requests\solvedRequest;
 use App\Models\Category;
 use App\Models\Complain;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class complainController extends Controller
 {
@@ -23,13 +25,20 @@ class complainController extends Controller
 
     }
 
+    public function categoryDelete(Request $req) {
+        Complain::where('categories_id', '=', $req->id)->delete();
+        Category::where('id', '=', $req->id)->delete();
+    }
+
 
     public function addComplaint(Request $req){
 
         $req->file('img')->store('public/old_ver_imgs');
         $image = $req->file('img')->hashName();
 
-        $complaint = Complain::create(['name' => $req->name, 'description' => $req->description, 'categories_id' => $req->category_id, 'users_id' => Auth::user()->id, 'image' => $image, 'status' => 'Новая']);
+        $complaint = Complain::create(['name' => $req->name, 'description' => $req->description, 
+                                        'categories_id' => $req->category_id, 'users_id' => Auth::user()->id, 
+                                        'image_old' => $image, 'image_new' => '', 'status' => 'Новая']);
 
         if($complaint){
             $success_message = $req->session()->put('success_message', 'Отправлено успешно!');
@@ -37,5 +46,34 @@ class complainController extends Controller
         }
 
     }
+
+
+    public function changeStatus(solvedRequest $req){
+
+        $req->file('image_new')->store('public/new_ver_imgs');
+        $img_name = $req->file('image_new')->hashName();
+        
+        Complain::where('id', '=', $req->id)->update(['status' => 'Решено', 'image_new' => $img_name]);
+
+        return redirect()->back();
+
+    }
+
+
+    public function decline(Request $req){
+
+
+        if($req->reason){
+            Complain::where('id', '=', $req->id)->update(['status' => 'Отклонена']);
+            return redirect()->back();
+
+        } 
+
+        $fail_message = $req->session()->put('fail_message', 'Необходимо заполнить поле!');
+        return redirect()->back()->with($fail_message);
+
+    }
+
+
 
 }
