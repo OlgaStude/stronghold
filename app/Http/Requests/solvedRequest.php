913917d71c59c1f40298f21ecb3d1c77aafea336
OlgaStude <112942851+OlgaStude@users.noreply.github.com>
2023-11-10 11:45:18 +0400

    Add files via upload

---
+++ app/Http/Requests/solvedRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class solvedRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'image_new' => 'required| mimes:jpeg,png'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            '*.required' => 'Необходимо заполнить поле',
+            'image.mimes' => 'Некорректный формат изображения'
+        ];
+    }
+}
